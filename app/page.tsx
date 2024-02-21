2aada53c57e54749153a636e9f76bed532a56f62
moil <moil1672@naver.com>
2024-02-21 11:29:05 +0900

    First commit 240221

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Hello This is HOME!</h1>
+    </div>
+  );
+}
+
+//Home
