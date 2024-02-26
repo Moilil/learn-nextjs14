7c601b118f1bcf1b5d7e72469cf5f9be5b3651db
moil <moil1672@naver.com>
2024-02-26 14:53:59 +0900

    240226 commit
    Before code challenge

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,16 +1,23 @@
 import { Suspense } from "react";
 import { API_URL } from "../../(home)/page";
-import MovieInfo from "../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+//fetching 할 수 있게 해주는 함수 > api로부터 영화 제목을 가져올 수 있음!
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
-      <h3>Movie detail page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
